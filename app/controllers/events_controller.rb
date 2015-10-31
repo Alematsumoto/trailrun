a97ef79862f17873316c1a28702b832a72c86b6e
Marcela Camargo <marcelacamargo@Air-de-Marcela.lan>
2015-10-31 19:27:39 -0300

    changes events presentation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:place_id, :date, :cost, :description, :conditions)
+      params.require(:event).permit(:place_id, :date, :cost, :description, :conditions, :name)
     end
 end
